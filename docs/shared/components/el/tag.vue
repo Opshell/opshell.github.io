1a70965b90352a7071c47c48857651e12d30fd69
Opshell <phenomx9990@gmail.com>
2026-01-31 00:01:23 +0800

    導入 @vueuse/core 14.1.0 版本 (需 install)

--- docs/shared/components/el/tag.vue
+++ docs/shared/components/el/tag.vue
@@ -6,35 +6,23 @@
 
 <template>
     <a class="el-tag" :href="`/tags-list.html?tag=${tag}`">
-        <span class="hash">#</span>
-        <span>{{tag}}</span>
-        <slot></slot>
+        #{{tag}}
     </a>
 </template>
 
 <style lang="scss">
     .el-tag {
-        @include setFlex();
-        gap: .25rem;
         background-color: color-mix(in srgb, var(--vp-c-brand) 20%, transparent);
-        padding: 6px 14px;
+        padding: 6px 15px;
         border-radius: 14px;
         color: var(--vp-c-brand-dark);
         font-size: .9rem;
         font-weight: 400;
         line-height: 1;
         transition: .2s var(--op-timing-FiSo);
-
-        .hash {
-            color: var(--vp-c-brand-1);
-            transition: .2s var(--op-timing-FiSo);
-        }
         &:hover {
-            .hash {
-                color: var(--color-extreme-reverse);
-            }
             background-color: var(--vp-c-brand);
-            color: var(--color-extreme-reverse);
+            color: white;
         }
     }
 </style>
